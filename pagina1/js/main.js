daafbb254669d98716abf9889826d91ad4ead03d
anne2023 <amabiledaiannecontato@gmail.com>
2023-02-22 22:39:59 -0300

    amabile

--- pagina1/js/main.js
+++ pagina1/js/main.js
@@ -1,1 +1,13 @@
-a
+const html = document.querySelector("html");
+const body = document.querySelector("body");
+var data = new Date();
+var horas = data.getHours();
+
+console.log(horas);
+
+window.addEventListener("load", function () {
+  if (horas < 18) {
+    html.classList.toggle("white-mode");
+    body.classList.toggle("white-mode");
+  }
+});
