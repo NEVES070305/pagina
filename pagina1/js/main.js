23d10c0b05e7ef5f66ee9a9aa6d6aace1474cc73
anne2023 <amabiledaiannecontato@gmail.com>
2023-02-22 22:56:44 -0300

    amabile

--- pagina1/js/main.js
+++ pagina1/js/main.js
@@ -1,3 +1,6 @@
+
+//copiei o codigo do outro grupo, para termos uma base apenas//
+
 const html = document.querySelector("html");
 const body = document.querySelector("body");
 var data = new Date();
